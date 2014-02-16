d1a887eace8c5167f8a3079fbe79e84c41fc3e8e
sergeyt <stodyshev@gmail.com>
2014-02-16 22:46:46 +0700

    modules builder

---
+++ cli.js
@@ -0,0 +1,11 @@
+﻿var fs = require('fs'),
+	path = require('path'),
+	yaml = require('js-yaml'),
+	build = require('./lib/build');
+
+// load build.yml and modules.yml
+var dir = process.cwd();
+var options = yaml.safeLoad(fs.readFileSync(path.join(dir, 'build.yml'), 'utf8'));
+var modules = yaml.safeLoad(fs.readFileSync(path.join(dir, 'modules.yml'), 'utf8'));
+
+build(options, modules);
